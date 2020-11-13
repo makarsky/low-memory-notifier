d41990a384cd5b68cc31428b3aef82c60a450e1a
Igor Makarsky <makarrski@gmail.com>
2020-11-13 09:25:38 +0300

    Add main script

---
+++ low_memory_notifier.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Minimum available memory limit, MB
+THRESHOLD=900
+
+#Check time interval, sec
+INTERVAL=10
+
+while :
+do
+
+    free=$(free -m|awk '/^Mem:/{print $4}')
+    buffers=$(free -m|awk '/^Mem:/{print $6}')
+    cached=$(free -m|awk '/^Mem:/{print $7}')
+    available=$(free -m | grep Mem | awk '{print $7}')
+
+    message="Free $free""MB"", buffers $buffers""MB"", cached $cached""MB"", available $available""MB"""
+
+    if [ $available -lt $THRESHOLD ]
+        then
+        notify-send "Memory is running out!" "$message"
+    fi
+
+    echo $message
+
+    sleep $INTERVAL
+
+done
+
